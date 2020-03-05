a2126cf0f4d4ec85df82f8e5fd1dacf76ad71431
Zihan Lin <lin@jacob.de>
2020-03-05 17:50:54 +0100

    Added name param to childBySelector and childrenBySelector to solve the multiple components mapping to one  problem.

--- src/Functions/ChildComponentFunctions.ts
+++ src/Functions/ChildComponentFunctions.ts
@@ -1,17 +1,18 @@
-import _ from "lodash";
 import { MultipleAccessor, SingleAccessor, VueComponent } from "../CommonTypes";
+import EasyVueTest from "../main";
 import { element, elements } from "./ElementFunctions";
 import { getWrappedObject } from "./HelperFunctions";
 
-export function childByName(componentName: string): SingleAccessor<VueComponent, VueComponent> {
+export function childByName(name: string): SingleAccessor<VueComponent, VueComponent> {
   return ({ wrapper }) => {
-    return getFilteredChildrenByNameRecursively(wrapper.do(getWrappedObject()), componentName)[0];
+    return findComponentByName(getAllChildrenComponents(wrapper), name);
   };
 }
 
-export function childBySelector(selector: string): SingleAccessor<VueComponent, VueComponent> {
+export function childBySelector(selector: string, name?: string): SingleAccessor<VueComponent, VueComponent> {
   return ({ wrapper }) => {
-    return (wrapper.get(element(selector)).do(getWrappedObject()) as any).__vue__;
+    const component = getOneComponentBySelector(wrapper, selector);
+    return name ? findComponentByName(getAllComponentsWithSameEl(component), name) : component;
   };
 }
 
@@ -21,37 +22,66 @@ export function childByRef(ref: string): SingleAccessor<VueComponent, VueCompone
   };
 }
 
-export function childrenByName(componentName: string): MultipleAccessor<VueComponent, VueComponent> {
+export function childrenByName(name: string): MultipleAccessor<VueComponent, VueComponent> {
   return ({ wrapper }) => {
-    return getFilteredChildrenByNameRecursively(wrapper.do(getWrappedObject()), componentName);
+    return filterComponentsByName(getAllChildrenComponents(wrapper), name);
   };
 }
 
-export function childrenBySelector(selector: string): MultipleAccessor<VueComponent, VueComponent> {
+export function childrenBySelector(selector: string, name?: string): MultipleAccessor<VueComponent, VueComponent> {
   return ({ wrapper }) => {
-    return wrapper.getAll(elements(selector)).map((e) => (e.do(getWrappedObject()) as any).__vue__);
+    return getAllComponentsBySelector(wrapper, selector)
+      .map((component) => name ? findComponentByName(getAllComponentsWithSameEl(component), name) : component);
   };
 }
 
-function getFilteredChildrenByNameRecursively(
-  component: VueComponent,
-  childComponentName: string,
-): VueComponent[] {
-  return _.filter(
-    getAllChildrenRecursively(component),
-    (childComponent) => (childComponent.$options as any)._componentTag === childComponentName,
-  );
+function getOneComponentBySelector(wrapper: EasyVueTest<VueComponent>, selector: string): VueComponent {
+  return (wrapper.get(element(selector)).do(getWrappedObject()) as any).__vue__;
 }
 
-function getAllChildrenRecursively(component: VueComponent): VueComponent[] {
+function getAllComponentsBySelector(wrapper: EasyVueTest<VueComponent>, selector: string): VueComponent[] {
+  return wrapper.getAll(elements(selector)).map((e) => (e.do(getWrappedObject()) as any).__vue__);
+}
+
+function getAllChildrenComponents(wrapper: EasyVueTest<VueComponent>): VueComponent[] {
+  return getAllChildrenComponentsRecursively(wrapper.do(getWrappedObject()));
+}
+
+function getAllComponentsWithSameEl(component: VueComponent): VueComponent[] {
+  const componentsWithSameEl: VueComponent[] = [component];
+
+  let currentComponent = component;
+  while (currentComponent.$parent && currentComponent.$parent.$el === currentComponent.$el) {
+    componentsWithSameEl.push(currentComponent.$parent);
+    currentComponent = currentComponent.$parent;
+  }
+
+  currentComponent = component;
+  while (currentComponent.$children.length === 1 && currentComponent.$children[0].$el === currentComponent.$el) {
+    componentsWithSameEl.push(currentComponent.$children[0]);
+    currentComponent = currentComponent.$children[0];
+  }
+
+  return componentsWithSameEl;
+}
+
+function getAllChildrenComponentsRecursively(component: VueComponent): VueComponent[] {
   let children: VueComponent[] = [];
 
   if (component.$children) {
     component.$children.forEach((child) => {
       children.push(child);
-      children = children.concat(getAllChildrenRecursively(child));
+      children = children.concat(getAllChildrenComponentsRecursively(child));
     });
   }
 
   return children;
+}
+
+function findComponentByName(components: VueComponent[], name: string): VueComponent {
+  return components.find((component) => (component.$options as any)._componentTag === name)!;
+}
+
+function filterComponentsByName(components: VueComponent[], name: string): VueComponent[] {
+  return components.filter((component) => (component.$options as any)._componentTag === name);
 }
--- test/Functions/ChildComponentFunctions.spec.ts
+++ test/Functions/ChildComponentFunctions.spec.ts
@@ -6,6 +6,7 @@ import EasyVueTest, {
   childrenByName,
   childrenBySelector,
   element,
+  getData,
   getTextContent,
 } from "../../src/main";
 
@@ -13,18 +14,40 @@ describe("ChildComponentFunctions", () => {
   let easy: EasyVueTest;
 
   beforeEach(async () => {
-    const childComponent = Vue.component("Child", {
+    const grandChildComponent = Vue.component("GrandChildComponent", {
       template: `
-        <div>
-          <p>Hello world!</p>
+        <div class="grand-child-component" >
+          <p>{{ message }}</p>
         </div>
       `,
+      props: {
+        message: { type: String, required: true },
+      },
+      data() {
+        return {
+          hiddenMessage: "I'm grand child component",
+        };
+      },
     });
 
-    const component = Vue.component("Parent", {
+    const childComponent = Vue.component("ChildComponent", {
+      template: `
+        <grand-child-component class="child-component" message="Hello world!" />
+      `,
+      components: {
+        grandChildComponent,
+      },
+      data() {
+        return {
+          hiddenMessage: "I'm child component",
+        };
+      },
+    });
+
+    const parentComponent = Vue.component("ParentComponent", {
       template: `
         <div>
-          <child-component id="child" ref="child"/>
+          <child-component class="child" ref="child"/>
         </div>
       `,
       components: {
@@ -32,7 +55,63 @@ describe("ChildComponentFunctions", () => {
       },
     });
 
-    easy = await EasyVueTest.mounted(component);
+    easy = await EasyVueTest.mounted(parentComponent);
+  });
+
+  it("gets child or children by name", () => {
+    expect(easy
+      .get(childByName("child-component"))
+      .get(element("p"))
+      .do(getTextContent()),
+    ).toEqual("Hello world!");
+
+    expect(easy
+      .getAll(childrenByName("child-component"))[0]
+      .get(element("p"))
+      .do(getTextContent()),
+    ).toEqual("Hello world!");
+  });
+
+  it("gets child or children by querying selector", () => {
+    expect(easy
+      .get(childBySelector(".child"))
+      .get(element("p"))
+      .do(getTextContent()),
+    ).toEqual("Hello world!");
+
+    expect(easy
+      .get(childBySelector(".child"))
+      .get(element("p"))
+      .do(getTextContent()),
+    ).toEqual("Hello world!");
+
+    expect(easy
+      .getAll(childrenBySelector(".child"))[0]
+      .get(element("p"))
+      .do(getTextContent()),
+    ).toEqual("Hello world!");
+  });
+
+  it("gets the child and children by querying selector and filtering by name", () => {
+    expect(easy
+      .get(childBySelector(".child", "child-component"))
+      .do(getData("hiddenMessage")),
+    ).toEqual("I'm child component");
+
+    expect(easy
+      .get(childBySelector(".child", "grand-child-component"))
+      .do(getData("hiddenMessage")),
+    ).toEqual("I'm grand child component");
+
+    expect(easy
+      .getAll(childrenBySelector(".child", "child-component"))[0]
+      .do(getData("hiddenMessage")),
+    ).toEqual("I'm child component");
+
+    expect(easy
+      .getAll(childrenBySelector(".child", "grand-child-component"))[0]
+      .do(getData("hiddenMessage")),
+    ).toEqual("I'm grand child component");
   });
 
   it("gets child component in different ways", () => {
@@ -43,7 +122,7 @@ describe("ChildComponentFunctions", () => {
     ).toEqual("Hello world!");
 
     expect(easy
-      .get(childBySelector("#child"))
+      .get(childBySelector(".child"))
       .get(element("p"))
       .do(getTextContent()),
     ).toEqual("Hello world!");
@@ -63,7 +142,7 @@ describe("ChildComponentFunctions", () => {
     ).toEqual("Hello world!");
 
     expect(easy
-      .getAll(childrenBySelector("#child"))[0]
+      .getAll(childrenBySelector(".child"))[0]
       .get(element("p"))
       .do(getTextContent()),
     ).toEqual("Hello world!");
